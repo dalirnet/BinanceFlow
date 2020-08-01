33ef61386510c62ae06bd105846c4009e7204c3b
AmirRezaDalir <dalirnet@gmail.com>
2020-08-01 21:26:33 +0430

    start

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import AtComponents from "at-ui";
+
+import "at-ui-style";
 
 Vue.config.productionTip = false;
+Vue.use(AtComponents);
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
